53df0d78242c0e065950f30e32e0ec9d9fdbd9c4
Hervé Guétin <herve.guetin@dnd.fr>
2025-02-22 22:35:18 +0100

    Homepage handling

---
+++ Plugin/Framework/Controller/ResultInterfacePlugin.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Maddlen\Nivo\Plugin\Framework\Controller;
+
+use Maddlen\Nivo\Framework\View\Result\Page;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Store\Model\ScopeInterface;
+
+class ResultInterfacePlugin
+{
+    public function __construct(
+        private readonly RequestInterface     $request,
+        private readonly ScopeConfigInterface $scopeConfig,
+    )
+    {
+    }
+
+    /**
+     * @param ResultInterface $subject
+     * @param ResponseInterface $response
+     * @return array
+     */
+    public function beforeRenderResult(ResultInterface $subject, ResponseInterface $response): array
+    {
+        if ($subject instanceof Page) {
+            $pageId = $this->scopeConfig->getValue(\Magento\Cms\Helper\Page::XML_PATH_HOME_PAGE, ScopeInterface::SCOPE_STORE);
+            $this->request->setParam('page_id', $pageId);
+        }
+        return [$response];
+    }
+}
